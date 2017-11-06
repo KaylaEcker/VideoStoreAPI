d4003b7a1509b27db676d21710f1e0cda583b364
Kayla Ecker <The Email Associated with Your Github Account>
2017-11-06 14:25:29 -0800

    passing tests for movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,10 @@
 class MoviesController < ApplicationController
   def index
+    movies = Movie.all
+    render(
+    json:movies.as_json(only: [:id, :title, :release_date]),
+    status: :ok
+    )
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  get 'movies/index'
-
-  get 'movies/show'
-
-  get 'movies/create'
-
-resources :customers, only: [:index, :show, :create]
+  
+resources :customers, :movies, only: [:index, :show, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 get '/zomg', to: 'application#zomg', as:'zomg'
 end
