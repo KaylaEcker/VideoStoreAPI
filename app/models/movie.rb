be9f1d7211f47699bd7ea8bc4687c8358ebd827b
murog <guillermina.muro@gmail.com>
2017-11-07 15:02:09 -0800

    add method to determine with a movie was available

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,7 +2,7 @@ class Movie < ApplicationRecord
   validates :title, presence: true
 
   def available?
-    if self.inventory > 0
+    if self.available_inventory > 0
       return true
     else
       return false
