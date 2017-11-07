dbea1611afca801af3f8ae4a5dec57f8b85b9a3e
murog <guillermina.muro@gmail.com>
2017-11-07 11:16:24 -0800

    fixed get checkout to post

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-get 'rentals/checkout', to: 'rentals#check_out', as: 'rentals_check_out'
+post 'rentals/checkout', to: 'rentals#check_out', as: 'rentals_check_out'
 resources :rentals
 resources :customers, :movies, only: [:index, :show, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
