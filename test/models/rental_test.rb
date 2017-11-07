f0fae0487ae891cf74c08e00b0c0c264e15b0254
murog <guillermina.muro@gmail.com>
2017-11-07 11:12:40 -0800

    passing validations for rental

---
+++ test/models/rental_test.rb
@@ -0,0 +1,22 @@
+require "test_helper"
+
+describe Rental do
+  let(:rental) { Rental.new }
+
+  it "is invalid without a customer and movie" do
+    rental.valid?.must_equal false
+    rental.customer_id.must_equal nil
+    rental.movie_id.must_equal nil
+    rental.errors.keys.must_include :customer && :movie
+  end
+
+  it "is invalid without a due_date" do
+    rental = rentals(:no_due_date)
+    rental.valid?.must_equal false
+  end
+
+  it "is invalid without a check_out_date" do
+    rental = rentals(:no_check_out_date)
+    rental.valid?.must_equal false
+  end
+end
