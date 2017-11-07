f4e703c029125192c56cd907ec629d2e077b309d
murog <guillermina.muro@gmail.com>
2017-11-07 14:03:20 -0800

    passing test for invalid and valid date responses

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,12 +1,18 @@
+require 'date'
+require "pry"
 class RentalsController < ApplicationController
   def check_out
     movie = Movie.find_by(id: params[:movie_id])
     customer = Movie.find_by(id: params[:customer_id])
     if movie && customer
-      rental = Rental.create(rental_params)
-      if rental.valid?
-        render json:rental.as_json(only:[:movie_id, :due_date, :check_out_date, :customer_id]),
+      rental = Rental.new(rental_params)
+      today = Date.today
+      today = today.strftime("%F")
+      rental.check_out_date = today
+      if rental.save
+        render json:rental.as_json(only:[:movie_id, :due_date, :check_out_date, :customer_id, :id]),
         status: :ok
+        return
       end
     end
     bad_request
--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -3,38 +3,71 @@ require "test_helper"
 describe RentalsController do
   let(:rental_data) {
     {
-      customer_id: 1,
-      movie_id: 1,
+      customer_id: customers(:one).id,
+      movie_id: movies(:one).id,
       due_date: "2018-12-25"
     }
   }
-describe "checkout" do
-  it "is a real working route" do
-    post rentals_check_out_path, params: {key: "value"}
-    must_respond_with :bad_request
+
+  let(:bad_rental_data) {
+    {
+      customer_id: 100,
+      movie_id: 100,
+      due_date: "lol"
+    }
+  }
+  describe "checkout" do
+    it "is a real working route" do
+      post rentals_check_out_path, params: {key: "value"}
+      must_respond_with :bad_request
+    end
+
+    it "returns json" do
+      post rentals_check_out_path, params: rental_data
+      response.header['Content-Type'].must_include 'json'
+    end
+
+    it "responds with success for a valid data" do
+      post rentals_check_out_path, params: rental_data
+      must_respond_with :success
+    end
+
+    it "creates a new rental when given valid data" do
+      proc {
+        post rentals_check_out_path, params: rental_data
+      }.must_change 'Rental.count', 1
+    end
+
+
+    it "returns a hash" do
+      post rentals_check_out_path, params: rental_data
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Hash
+    end
+
+    it "returns bad_request for invalid requests" do
+      post rentals_check_out_path, params: bad_rental_data
+      must_respond_with :bad_request
+    end
+
+    it "doesn't create a new rental when given invalid data" do
+      proc {
+        post rentals_check_out_path, params: bad_rental_data
+      }.must_change 'Rental.count', 0
+    end
+
+    it "returns bad_request if movie doesn't have inventory" do
+      
+    end
+
+    it "returns a rental with exactly the correct required fields" do
+      keys = %w(check_out_date customer_id due_date id movie_id)
+      post rentals_check_out_path, params: rental_data
+      body = JSON.parse(response.body)
+      body.keys.sort.must_equal keys
+      body.must_be_instance_of Hash
+      body.count.must_equal 5
+    end
   end
-
-  it "returns json" do
-    post rentals_check_out_path, params: rental_data
-    response.header['Content-Type'].must_include 'json'
-  end
-
-  it "returns an array" do
-
-  end
-
-  it "returns bad_request for invalid requests" do
-
-  end
-
-  it "returns bad_request if movie doesn't have inventory" do
-
-  end
-
-  it "returns a rental with exactly the correct required fields" do
-
-  end
-
-end
 
 end
