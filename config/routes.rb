6d35a00dfc775d9f15e7c4ef049f5baad2f47538
murog <guillermina.muro@gmail.com>
2017-11-07 11:14:41 -0800

    updated checkout path to point to checkout action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  
+
+get 'rentals/checkout', to: 'rentals#check_out', as: 'rentals_check_out'
+resources :rentals
 resources :customers, :movies, only: [:index, :show, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 get '/zomg', to: 'application#zomg', as:'zomg'
