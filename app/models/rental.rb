65c38651ce3d8803b72e23b8d0bbc85a224027b2
murog <guillermina.muro@gmail.com>
2017-11-07 15:03:05 -0800

    commented out validation for movie available

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -3,4 +3,15 @@ class Rental < ApplicationRecord
   belongs_to :movie
   validates :due_date, presence: true
   validates :check_out_date, presence: true
+  # validate :movie_available?, :on => :save
+  #
+  # def movie_available?
+  #   movie = self.movie
+  #   if movie
+  #     if movie.available?
+  #       return true
+  #     end
+  #   end
+  #   return false
+  # end
 end
