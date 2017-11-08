c59fc909a406932d22b9a05807a420b66f92e115
murog <guillermina.muro@gmail.com>
2017-11-08 10:14:01 -0800

    fixed typo in validation and added self.errors

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -3,15 +3,12 @@ class Rental < ApplicationRecord
   belongs_to :movie
   validates :due_date, presence: true
   validates :check_out_date, presence: true
-  # validate :movie_available?, :on => :save
-  #
-  # def movie_available?
-  #   movie = self.movie
-  #   if movie
-  #     if movie.available?
-  #       return true
-  #     end
-  #   end
-  #   return false
-  # end
+  validate :movie_available?, :on => :create
+
+  def movie_available?
+    movie = self.movie
+    if movie && !(movie.available?)
+      self.errors[:movie] << "does not have any available inventory."
+    end
+  end
 end
